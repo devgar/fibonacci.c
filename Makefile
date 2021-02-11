85324e1d5b2fa167449ca438a90c78ed42549153
Edgar Albalate <dev.gardo@gmail.com>
2021-02-11 15:52:13 +0100

    Added command line arguments

--- Makefile
+++ Makefile
@@ -6,3 +6,4 @@ run: a.out
 	./a.out
 a.out: main.c
 	gcc main.c
+build: a.out
--- main.c
+++ main.c
@@ -2,11 +2,14 @@
 
 int a, b, i, sum, input;
 
-int main () {
+int main (int argc, char * argv[]) {
 
-  printf("Gice me a num ");
-  scanf("%d", &input);
-
+  if (argc > 1)
+    sscanf(argv[1], "%d", &input);
+  else { 
+    printf("Give me a num ");
+    scanf("%d", &input);
+  }
   for(i = 0; i < input; i++) {
     if (i < 2) sum = 1;
     else sum = a + b;
